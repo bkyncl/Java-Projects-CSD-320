4e194f7bedaa9000b14be772d9b66030bc8359a9
Brittany Kyncl <brittanyrosekyncl@gmail.com>
2022-10-31 17:20:18 -0500

    "new"

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -9,6 +9,7 @@
  * 3 = Scissors
  * */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class RockPaperScissors {
@@ -17,16 +18,16 @@ public class RockPaperScissors {
         
         Scanner input = new Scanner(System.in);
         String player; // player name
-        int computerChoice = (int)(Math.random() * 3 + 1); // randomly generate computer choice 1-3
+        int computerChoice; // computer choice
         int playerChoice; // player choice
-        int playAgain;
-
+        int playAgain = 1;
+    
         // program message
         System.out.println("\nWelcome to Rock, Paper, and Scissors game!!!");
         System.out.print("Please enter player name: "); // get user's name
         player = input.nextLine();
-
-        do {
+    
+        while (playAgain == 1) {
 
             // player options prompt
             System.out.println("\nWelcome " + player + "!" + "\nPlease choose your weapon..."
@@ -38,9 +39,9 @@ public class RockPaperScissors {
                 computerChoice = (int)(Math.random() * 3 + 1); // randomly generate computer choice 1-3
 
                 System.out.print("\nEnter choice: "); // user choice prompt
-
-                playerChoice = input.nextInt();
                 
+                playerChoice = input.nextInt();
+
                 if(playerChoice == computerChoice) { // check for tie
                     System.out.println("\nYou Tied. Try again!");// let user try again
                 }
@@ -53,18 +54,27 @@ public class RockPaperScissors {
             
             }
 
+            String playerWeapon = Integer.toString(playerChoice);
+            String computerWeapon = Integer.toString(computerChoice);
+
             // switching player choice 1-3 to cooresponding weapon options for print statement
             switch (playerChoice) {
-                case 1: System.out.println("\n" + player +  " chose: \"ROCK\""); break;
-                case 2: System.out.println("\n" + player +  " chose: \"PAPER\""); break;
-                case 3: System.out.println("\n" + player +  " chose: \"SCISSORS\""); break;
+                case 1: System.out.println("\n" + player +  " chose: \"ROCK\"");
+                        playerWeapon = "ROCK"; break;
+                case 2: System.out.println("\n" + player +  " chose: \"PAPER\""); 
+                        playerWeapon = "PAPER"; break;
+                case 3: System.out.println("\n" + player +  " chose: \"SCISSORS\"");
+                        playerWeapon = "SCISSORS"; break;
             }
 
             // switching copmuter choice 1-3 to cooresponding weapon options for print statement
             switch (computerChoice) {
-                case 1: System.out.println("Your oppenent chose: \"ROCK\""); break;
-                case 2: System.out.println("Your oppenent chose: \"PAPER\""); break;
-                case 3: System.out.println("Your oppenent chose: \"SCISSORS\""); break;
+                case 1: System.out.println("Your oppenent chose: \"ROCK\"");
+                        computerWeapon = "ROCK"; break;
+                case 2: System.out.println("Your oppenent chose: \"PAPER\"");
+                        computerWeapon = "PAPER"; break;
+                case 3: System.out.println("Your oppenent chose: \"SCISSORS\"");
+                        computerWeapon = "SCISSORS"; break;
             }
 
             // check player choice against computerchoice
@@ -72,19 +82,20 @@ public class RockPaperScissors {
                     || playerChoice == 3 && computerChoice == 2
                     || playerChoice == 2 && computerChoice == 1) {
                 System.out.println("--- YOU WON!! ---");// winning statement
+                System.out.println(playerWeapon + " beats " + computerWeapon);
             }
             else {
                 System.out.println("--- You lost :( ---"); // losing statement
+                System.out.println(computerWeapon + " beats " + playerWeapon);
             }
         
             System.out.print("\nTo play again enter 1 to exit press 2: ");
             playAgain = input.nextInt();
 
-        } while(playAgain == 1);
+        }
 
         System.out.println("Goodbye...");
 
         input.close();
         
-    }
 }
