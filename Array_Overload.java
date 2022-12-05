49f22d3a6abd0dfebd50f41eb3ef819159fb088c
Brittany Kyncl <brittanyrosekyncl@gmail.com>
2022-12-05 13:21:01 -0600

    "new edits"

--- Array_Overload.java
+++ Array_Overload.java
@@ -23,46 +23,44 @@ public class Array_Overload {
         int lng[] = new int[4]; // array to hold array data type variable based on user input
         int a,b,c,d; // variables to hold each data type for arrays
 
-            // get desired length of arrays for each array data type
-            System.out.println("Enter desired length of each array data type..."
-                            + "\n1. Short"
-                            + "\n2. Integer"
-                            + "\n3. Long"
-                            + "\n4. Double\n"); 
-            for(int i = 0; i< lng.length; i ++){ // loop to take in user input for data type holder array
-                System.out.print(i + 1 +": ");
-                try{
-                    lng[i]= input.nextInt();
-                    if(lng[i] < 1){ // if less than 1 reset counter
-                        i -= 1;
-                    }
-                }
-                catch (InputMismatchException e) { // exception on input
-                    input.nextLine();
-                    i -= 1; // reset counter
+        // get desired length of arrays for each array data type
+        System.out.println("Enter desired length of each array data type..."
+                        + "\n1. Short"
+                        + "\n2. Integer"
+                        + "\n3. Long"
+                        + "\n4. Double\n"); 
+        for(int i = 0; i< lng.length; i ++){ // loop to take in user input for data type holder array
+            System.out.print(i + 1 +": ");
+            try{
+                lng[i]= input.nextInt();
+                if(lng[i] < 1){ // if less than 1 reset counter
+                    i -= 1;
                 }
             }
-            a=lng[0]; // assign value at index of data type array to cooresponding variable
-            b=lng[1]; // assign value at index of data type array to cooresponding variable
-            c=lng[2]; // assign value at index of data type array to cooresponding variable
-            d=lng[3]; // assign value at index of data type array to cooresponding variable
-            System.out.println(a + b + c + d);
-                    
-            // get array range for random generation
-            while(true){
-                System.out.print("Now enter desired range of array number generation. From 0 - ");
-                try{
-                    rng = input.nextInt(); // store array range 
-                    if(rng < 0){
-                        continue;
-                    }
-                    break;
-                }
-                catch (InputMismatchException e) { // exception on input
-                    input.nextLine();
-                }
+            catch (InputMismatchException e) { // exception on input
+                input.nextLine();
+                i -= 1; // reset counter
             }
- 
+        }
+        a=lng[0]; // assign value at index of data type array to cooresponding variable
+        b=lng[1]; // assign value at index of data type array to cooresponding variable
+        c=lng[2]; // assign value at index of data type array to cooresponding variable
+        d=lng[3]; // assign value at index of data type array to cooresponding variable
+
+        // get array range for random generation
+        while(true){
+            System.out.print("Now enter desired range of array number generation. From 0 - ");
+            try{
+                rng = input.nextInt(); // store array range 
+                if(rng < 0){
+                    continue;
+                }
+                break;
+            }
+            catch (InputMismatchException e) { // exception on input
+                input.nextLine();
+            }
+        }
         // calling methods to generate random arrays parsing cooresponding array size and number gen. range
         short[] shortArray = Methods.randomShrtArray(a, rng);
         int[] intArray = Methods.randomIntArray(b, rng);
@@ -83,7 +81,6 @@ public class Array_Overload {
         System.out.println("Double array AVG: " + Methods.getAverage(doubleArray));
         System.out.println();
 
-        System.out.println(Methods.randomRange(rng));
         input.close();
     }
 }
