99e84ab0288b0b88f872110c1e0c6fa2e87fe9b7
Brittany Kyncl <brittanyrosekyncl@gmail.com>
2022-12-08 20:14:13 -0600

    final

--- Two_Dim_Array.java
+++ Two_Dim_Array.java
@@ -10,20 +10,40 @@ import java.util.*;
 
 public class Two_Dim_Array {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         // function message
         System.out.println("\nDisplaying two dimensional arrays and the indices of the min/max element locations..\n");
 
-        // Displaying and finding min max on int type 2d array
+        int numberOfRows, numberOfColumns; // vars to hold user inputs
+        // promting user for 2d array num of rows and columns
+        System.out.print("Enter desired number of rows in 2D array: ");
+        numberOfRows = input.nextInt();
+        System.out.print("Enter desired number of columns in 2D array: ");
+        numberOfColumns = input.nextInt();
+
         // declaring int type 2d array filled with random numbers retunred from array generation method
-        int [][] intArray = intArray();
-        printArray(intArray);// calling print method for display
-        System.out.println();
+        int [][] intArray = intArray(numberOfRows, numberOfColumns); // parsing user input
+        // declaring double type 2d array filled with random numbers retunred from array generation method
+        double [][] doubleArray = doubleArray(numberOfRows, numberOfColumns); //parsing user input
+
         // declaring array to hold indices of largest element location returned from method
         int[] max = locateLargest(intArray); 
         // declaring array to hold indices of smallest element location returned from method
         int[] min = locateSmallest(intArray);
 
-        // displaying results
+        // declaring array to hold indices of largest element location returned from method
+        int[] max2 = locateLargest(doubleArray);
+        // declaring array to hold indices of smallest element location returned from method
+        int[] min2 = locateSmallest(doubleArray);
+
+
+        // Displaying and finding min max on int type 2d array
+        System.out.println();
+        printIndArray(intArray); // calling print method for display
+        System.out.println();
+
+        // displaying results for int type 2d array
         System.out.println("The max element is located at row index, column index: " + Arrays.toString(max));
         System.out.println("Max Element value: " +intArray[max[0]][max[1]]); // displaying value held at max location
         System.out.println("The min element is located at row index, column index: " + Arrays.toString(min));
@@ -31,21 +51,17 @@ public class Two_Dim_Array {
         System.out.println();
 
         // Displaying and finding min max on doubly type 2d array
-        // declaring double type 2d array filled with random numbers retunred from array generation method
-        double [][] doubleArray = doubleArray();
-        printArray(doubleArray); // calling print method for display
+        printIndArray(doubleArray); // calling print method for display
         System.out.println();
-        // declaring array to hold indices of largest element location returned from method
-        int[] max2 = locateLargest(doubleArray);
-        // declaring array to hold indices of smallest element location returned from method
-        int[] min2 = locateSmallest(doubleArray);
-
-        // displaying results
+        
+        // displaying results for double type 2d array
         System.out.println("The max element is located at row index, column index: " + Arrays.toString(max2));
         System.out.println("Max Element value: " + doubleArray[max2[0]][max2[1]]); // displaying value held at max location
         System.out.println("The min elementis located at row index, column index: " + Arrays.toString(min2));
         System.out.println("Min Element value: " + doubleArray[min2[0]][min2[1]]); // displaying value held at min location
         System.out.println();
+
+        input.close();
         
     } 
     // method to return location of largets value of 2d array paramenter
@@ -101,9 +117,9 @@ public class Two_Dim_Array {
 		return min;
     } 
     //method to fill int type 2d array with randomized values
-    public static int[][] intArray() {
+    public static int[][] intArray(int x, int y) {
         Random rand = new Random();
-        int[][] array = new int[2][5];
+        int[][] array = new int[x][y];
         // fill array with random numbers
         for(int i = 0; i < array.length; i++){
             for(int j = 0; j < array[i].length; j++){
@@ -113,9 +129,9 @@ public class Two_Dim_Array {
         return array;
     }
     //method to fill double type 2d array with randomized values
-    public static double[][] doubleArray() {
+    public static double[][] doubleArray(int x, int y) {
         Random rand = new Random();
-        double[][] array = new double[2][5];
+        double[][] array = new double[x][y];
         // fill array with random numbers
         for(int i = 0; i < array.length; i++){
             for(int j = 0; j < array[i].length; j++){
@@ -126,9 +142,11 @@ public class Two_Dim_Array {
     }
     public static void printArray(int[][] arrayParam) {
         // method to print 2d  array row by row with formatting
+        System.out.println("----------------------------------");
         System.out.println("Int 2D Array Elements: ");
+        System.out.println("----------------------------------");
         for(int i = 0; i < arrayParam.length; i++){
-            System.out.print("[");
+            System.out.print("Row " + i + " |  [");
             for(int j = 0; j < arrayParam[i].length; j++)
                 if(j < arrayParam[i].length -1)
                     System.out.print(arrayParam[i][j] + ", ");
@@ -139,9 +157,11 @@ public class Two_Dim_Array {
     }
     public static void printArray(double[][] arrayParam) {
         // method to print 2d  array row by row with formatting
+        System.out.println("----------------------------------");
         System.out.println("Double 2D Array Elements: ");
+        System.out.println("----------------------------------");
         for(int i = 0; i < arrayParam.length; i++){
-            System.out.print("[");
+            System.out.print("Row " + i + " |  [");
             for(int j = 0; j < arrayParam[i].length; j++)
                 if(j < arrayParam[i].length -1)
                     System.out.print(arrayParam[i][j] + ", ");
@@ -150,20 +170,27 @@ public class Two_Dim_Array {
              System.out.println("]");
         }
     }
-    public static void printRow(int[][] arrayParam) {
-        // method to print 2d  array row by row
+    public static void printIndArray(int[][] arrayParam) {
+        System.out.println("----------------------------------");
+        System.out.println("Int 2D Array Elements: ");
+        System.out.println("----------------------------------");
         for(int i = 0; i < arrayParam.length; i++){
-            for(int j = 0; j < arrayParam[i].length; j++)
-                System.out.print(arrayParam[i][j] + " ");
-             System.out.println();
+            for(int j = 0; j < arrayParam[i].length; j++){
+                System.out.print("[" + i + "][" + j + "] = " + arrayParam[i][j] + " ");
+            }
+            System.out.println();
         }
     }
-    public static void printColumn(int[][] arrayParam) {
-        // method to print 2d array column by column
+    public static void printIndArray(double[][] arrayParam) {
+        System.out.println("----------------------------------");
+        System.out.println("Double 2D Array Elements: ");
+        System.out.println("----------------------------------");
         for(int i = 0; i < arrayParam.length; i++){
-            for(int j = 0; j < arrayParam.length; j++)
-                System.out.print(arrayParam[j][i] + " ");
-             System.out.println();
+            for(int j = 0; j < arrayParam[i].length; j++){
+                System.out.print("[" + i + "][" + j + "] = " + arrayParam[i][j] + " ");
+            }
+            System.out.println();
         }
     }
+       
 }
